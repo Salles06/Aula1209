743c39f49f5d4fb0fe4a0c1f94ddf8f868677410
Salles06 <joaovictorsalesdepaula@gmail.com>
2024-09-12 19:34:27 -0300

    Desafio Manipulação Dicionarios e Objetos

---
+++ aula1209.py
@@ -0,0 +1,9 @@
+# Manipulação de Dicionarios/Objetos
+
+livro = {"Autor":"Sales", "ano": 2021, "titulo":"Abraço pode salvar vidas!"}
+print(livro)
+
+adicionar = input("Qual a data de lançamento?")
+
+livro["ano"] = adicionar
+print("Os dados atualizados são ", livro)
